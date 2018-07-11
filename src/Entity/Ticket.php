dea1573c7e8b0f2b56ac548927fcad7d65830c86
Svetlana Muraveckaja-Odincova <svetlana.mur.85@gmail.com>
2018-07-11 11:44:28 +0300

    Create entity Genre

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TicketRepository")
+ */
+class Ticket
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $genre;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $tickets_left;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $tickets_available;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getGenre(): ?int
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(int $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTicketsLeft(): ?int
+    {
+        return $this->tickets_left;
+    }
+
+    public function setTicketsLeft(?int $tickets_left): self
+    {
+        $this->tickets_left = $tickets_left;
+
+        return $this;
+    }
+
+    public function getTicketsAvailable(): ?int
+    {
+        return $this->tickets_available;
+    }
+
+    public function setTicketsAvailable(int $tickets_available): self
+    {
+        $this->tickets_available = $tickets_available;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
